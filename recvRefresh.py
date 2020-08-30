55fb6c28d2bb822c36626e3b41268ea0fbe79ab7
lsk-china <55624809+lsk-china@users.noreply.github.com>
2020-08-30 21:06:01 +0800

    commit

--- recvRefresh.py
+++ recvRefresh.py
@@ -1,9 +1,5 @@
 import os
-import time
-
-while True:
-	os.system("git pull && git merge")
-	os.system("git add .")
-	os.system("git commit  -m \"commit\"")
-	os.system("git push origin -u master")
-	time.sleep(60*60*12)
+os.system("git pull && git merge")
+os.system("git add .")
+os.system("git commit  -m \"commit\"")
+os.system("git push origin -u master")
